05f4243fefc04647dc50dc12e5274391363d8a08
Orlando Vidaurre <orlandovidaurre@MacBook-Pro-de-Orlando.local>
2023-04-09 13:29:54 +0200

    Deployment de la App

---
+++ classes/email.php
@@ -0,0 +1,80 @@
+<?php  
+
+namespace Classes;
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+class Email{
+    public $email;
+    public $nombre;
+    public $token;
+    public function __construct($email, $nombre, $token)
+    {
+        $this->email = $email;
+        $this->nombre = $nombre;
+        $this->token = $token;
+    }
+    public function enviarConfirmacion(){
+        // crear el objeto de email
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = 'sandbox.smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username = 'bfdfd650909afb';
+        $mail->Password = '4ca154ab6ca6e0';
+
+        $mail->setFrom('cuentas@appbarber.com');
+        $mail->addAddress('cuentas@appbarber.com', 'AppBarber.com');
+        $mail->Subject = "Confirma tu cuenta";
+
+        // set HTML
+
+        $mail->isHTML(TRUE);
+        $mail->CharSet = "UTF-8";
+
+        $contenido = '<html>';
+        $contenido .= "<p><strong>Hola " . $this->email . "</strong> Has creado tu cuenta ahora eres un Vikingo, solo debes confirmarla presionando el siguiente enlace </p>";
+        $contenido .= "<p> Presiona aqui: <a href='http://localhost:3008/confirmar-cuenta?token=" . $this->token . "'>Confirmar cuenta</a></p>";
+        $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
+        $contenido .= '</html>';
+
+        $mail->Body = $contenido;
+
+        //enviar email
+
+        $mail->send();
+    
+    }
+    public function enviarInstrucciones(){
+          // crear el objeto de email
+          $mail = new PHPMailer();
+          $mail->isSMTP();
+          $mail->Host = 'sandbox.smtp.mailtrap.io';
+          $mail->SMTPAuth = true;
+          $mail->Port = 2525;
+          $mail->Username = 'bfdfd650909afb';
+          $mail->Password = '4ca154ab6ca6e0';
+  
+          $mail->setFrom('cuentas@appbarber.com');
+          $mail->addAddress('cuentas@appbarber.com', 'AppBarber.com');
+          $mail->Subject = "Restablece tu Password";
+  
+          // set HTML
+  
+          $mail->isHTML(TRUE);
+          $mail->CharSet = "UTF-8";
+  
+          $contenido = '<html>';
+          $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has solicitado resstablecer tu password, sigue en el siguiente enlace para hacerlo. </p>";
+          $contenido .= "<p> Presiona aqui: <a href='http://localhost:3008/recuperar?token=" . $this->token . "'>Restablecer Password</a></p>";
+          $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
+          $contenido .= '</html>';
+  
+          $mail->Body = $contenido;
+  
+          //enviar email
+  
+          $mail->send();
+    }
+}
