09077cf74e4063e2dbb05c236b8ca84c0488afa3
Orlando Vidaurre <126068352+OJVV@users.noreply.github.com>
2023-04-10 00:18:19 +0200

    Update Email.php

--- classes/Email.php
+++ classes/Email.php
@@ -18,11 +18,12 @@ class Email{
         // crear el objeto de email
         $mail = new PHPMailer();
         $mail->isSMTP();
-        $mail->Host = 'sandbox.smtp.mailtrap.io';
+        $mail->Host = $_ENV['MAIL_HOST'];
         $mail->SMTPAuth = true;
-        $mail->Port = 2525;
-        $mail->Username = 'bfdfd650909afb';
-        $mail->Password = '4ca154ab6ca6e0';
+        $mail->Username = $_ENV['MAIL_USER'];
+        $mail->Password = $_ENV['MAIL_PASSWORD'];
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = $_ENV['MAIL_PORT'];
 
         $mail->setFrom('cuentas@appbarber.com');
         $mail->addAddress('cuentas@appbarber.com', 'AppBarber.com');
@@ -35,7 +36,7 @@ class Email{
 
         $contenido = '<html>';
         $contenido .= "<p><strong>Hola " . $this->email . "</strong> Has creado tu cuenta ahora eres un Vikingo, solo debes confirmarla presionando el siguiente enlace </p>";
-        $contenido .= "<p> Presiona aqui: <a href='http://localhost:3008/confirmar-cuenta?token=" . $this->token . "'>Confirmar cuenta</a></p>";
+        $contenido .= "<p>Presiona aquí: <a href='" . $_ENV['SERVER_HOST'] . "confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta </a> </p>";
         $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
         $contenido .= '</html>';
 
